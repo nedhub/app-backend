e3c921c0e5816a037880465838ce76e00bc1e393
Ned Ememanka <nedememanka@icloud.com>
2021-04-19 03:33:23 -0400

    make it better

---
+++ server.js
@@ -0,0 +1,81 @@
+import express from "express";
+import mongoose from "mongoose";
+import data from './data.js';
+import videos from './dbModel.js';
+
+
+
+
+
+
+// app config
+const app = express();
+const port = 9000;
+
+
+//middlewares
+
+app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeaders('Access-Control-Allow-Origin', '*'),
+    res.setHeaders('Access-Control-Allow-Headers', '*'),
+    next();
+
+})
+
+
+//DB config
+
+const connection_url = "mongodb+srv://admin:6fIXZBnFltWd1TgZ@cluster0.5m9km.mongodb.net/tiktok?retryWrites=true&w=majority";
+
+
+
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+
+
+
+// api endpoints
+
+
+app.get('/', (req,res) =>res.status(200).send('hello world'));
+
+app.get('/v1/posts', (req, res) => res.status(200).send(data));
+
+app.get('/v2/posts', (req,res) => {
+    videos.find((err, data) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    })
+})
+
+app.post('/v2/posts', (req, res)=> {
+
+    // ADDING DATA TO THE DATABASE
+    const dbVideos = req.body;
+
+
+    videos.create(dbVideos, (err, data) => {
+        if(err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
+
+//admin mongodb username
+//6fIXZBnFltWd1TgZ mongodb password
+
+
+//listner
+
+
+
+app.listen(port, ()=>console.log(`listening on localhost:${port}`));
